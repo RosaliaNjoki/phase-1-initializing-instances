619869c8cbaa991da988c5a6c6fc31c10d310fcd
RosaliaNjoki <njosali0@gmail.com>
2024-02-16 13:34:55 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your code here
+class Breakfast{
+    constructor(food, drink){
+        this.food = food; 
+        this.drink = drink;
+
+    }
+}
+const bfast = new Breakfast("eggs", "juice");
+bfast.food;
+bfast.drink;
+
+class Lunch {
+    constructor(salad, soup, drink){
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+
+    }
+}
+const newlunch = new Lunch("side salad",
+"broccoli cheddar soup",
+"iced tea");
+newlunch.salad;
+newlunch.soup;
+newlunch.drink;
+
+ class Dinner{
+    #dessert;
+    constructor(salad, soup, entree, dessert){
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.#dessert = dessert;
+    }
+ }
+ const newdinner = new Dinner(
+    "balsamic salad",
+    "consomme",
+    "filet mignon",
+    "cheesecake"
+  );
+  newdinner.salad;
+  newdinner.soup;
+  newdinner.entree;
+ 
